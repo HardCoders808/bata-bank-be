802087d5334c2c6e587a052daad15df463a6bf3d
Antonin Vychodil <avychodil@monetplus.cz>
2026-03-03 11:12:21 +0100

    initial commit

---
+++ src/main/java/hardcoders808/backend/BataBankBeApplication.java
@@ -0,0 +1,13 @@
+package hardcoders808.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BataBankBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BataBankBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hardcoders808/backend/BataBankBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hardcoders808.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BataBankBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
