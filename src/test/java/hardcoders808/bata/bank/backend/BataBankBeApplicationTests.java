e9f51ec1b0f7133627bd1a2f67751c01f78a1528
mfataka <mfataka@monetplus.cz>
2026-03-03 16:20:00 +0100

    refactor keys

--- src/test/java/hardcoders808/bata/bank/backend/BataBankBeApplicationTests.java
+++ src/test/java/hardcoders808/bata/bank/backend/BataBankBeApplicationTests.java
@@ -1,13 +1,11 @@
 package hardcoders808.bata.bank.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BataBankBeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
